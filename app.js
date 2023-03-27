75e6374a68edc7134b3b211bde0c93a03b7a0eb3
nathsten <nathsten03@gmail.com>
2023-03-27 21:16:53 +0200

    Mail api

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const updateFunds = require('./modules/FantacyFond.js');
 const updateFintechFantacy = require('./modules/Yahoo.js');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const sgMail = require('@sendgrid/mail');
 dotenv.config({path: './.env'});
 const scedule = require('node-schedule');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false})); // Endre til ture hvis bruk av cookie-parser
+app.use(bodyParser.json())
 app.use('/', express.static('./dist'));
 
 const PWD = process.env.PWD;
@@ -18,11 +22,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, e => e ? console.log(e) : console.log(`Server running on http://127.0.0.1:${PORT}`));
 
+// Koble til Database
 client.connect()
 .then(() => console.log(`Client connected`))
 .catch(() => console.log(`Mangler API kobling, vennligst spesifiser i egen .env fil.`));
 
+// Koble til SendGrid Epost API.
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 // Henter data fra DN fantacy fond hvert 15-ene minutt
+/** Dette skal med i produksjon når det er nytt Fantacy fond konkuranse.
+ * Skal også lage en egen liknende funksjon for å hente data fra shareville. 
 const rule = new scedule.RecurrenceRule();
 rule.minute = new scedule.Range(0,59, 15);
 const job = scedule.scheduleJob(rule, async () => {
@@ -36,13 +46,14 @@ const job = scedule.scheduleJob(rule, async () => {
         // Sjekker at det er en ukedag (man-fre) mellom kl. 09:00 og 17:00. 
         if(HOUR >= 9 && HOUR <= 16 && DAY >= 1 && DAY <= 5)
             // I produksjon tas med!
-            // await updateFintechFantacy();
+            // await updateFintechFantacy(client);
             console.log("Getting funds...");
     }
     catch(err){
         console.log(err);
     }
 });
+*/
 
 // GET request for å sende fondsdata til client
 app.get('/getFantacyFunds', async (req, res) => {
@@ -55,3 +66,34 @@ app.get('/getFantacyFunds', async (req, res) => {
         console.log(err);
     }
 });
+
+// Kontakte oss via forum på nettsiden.
+app.post('/KontaktFintechEnigma', async (req, res) => {
+    const { navn, epost, tittel, melding } = req.body;
+    try{
+        const msg = {
+            to: 'fintechenigma@gmail.com', // Change to your recipient
+            from: 'fintechenigma@gmail.com', // Change to your verified sender
+            subject: `Ny kontakt fra ${navn} | ${tittel}`,
+            html: `
+                <strong>${tittel}</strong>
+                <p>${melding}</p>
+                <br>
+                <p>Sendt fra: ${navn}</p>
+                <p>Epost: ${epost}</p>
+            `,
+          }
+          sgMail.send(msg)
+          .then(() => console.log("Mail Sendt"))
+          .catch(err => {
+            const error = new ErrorEvent(err);
+            throw error;
+          });
+    
+          res.send({status: "OK"});
+    }
+    catch(error){
+        console.log(error);
+        res.send({status: "Det oppsto en feil, vennligst oppdater siden og prøv på nytt."});
+    }
+})
