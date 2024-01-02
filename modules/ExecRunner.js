6d514126534badd29ab2e75cc8d2f3fd96e8de49
nathsten <nathsten03@gmail.com>
2024-01-02 13:44:51 +0100

    lagt til file

---
+++ modules/ExecRunner.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process');
+
+/**
+ * @param {string} file 
+ * @param {string[]} args 
+ * @returns {Promise<string>}
+ */
+const ExecRunner = (file, args) => new Promise((resolve, reject) => {
+    try{
+
+        const promt = `python3 ./python/${file} ${args.filter(e => e !== undefined).join(" ")}`;
+
+        exec(promt, (error, stdout, stderr) => {
+            if (error) throw error;
+            resolve(stdout);
+            return;
+            
+        })
+    }
+    catch(error){
+        reject(error);
+        return;
+    }
+});
+
+module.exports = ExecRunner;
